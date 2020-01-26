2f499029382b7b1dbf6d8897672abb298426466f
DESKTOP-MSG9PH7\Shivam <contact@shivam.me>
2020-01-26 11:40:31 +0530

    Added exercise and user models

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cora = require("cors");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 require("dotenv").config();
@@ -11,7 +11,12 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
