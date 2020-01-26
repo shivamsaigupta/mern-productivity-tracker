ae9190c817bcda5375755cfcd5ef6adae310df62
DESKTOP-MSG9PH7\Shivam <contact@shivam.me>
2020-01-26 15:29:16 +0530

    added models

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,22 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
 
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
+
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
