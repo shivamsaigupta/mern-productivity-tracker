bd673181c69e0d1d0fb6c126f0641030d2e0657c
DESKTOP-MSG9PH7\Shivam <contact@shivam.me>
2020-01-27 21:02:30 +0530

    Connected Create Exercise and Create User with the Backend

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class EditExercise extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello from Edit Exercise</p>
+      </div>
+    );
+  }
+}
+
+export default EditExercise;
